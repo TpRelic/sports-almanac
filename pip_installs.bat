4bc14f49795c6a02853a026fe6d835d8f3dae034
TpRelic <62119566+TpRelic@users.noreply.github.com>
2025-05-05 20:25:27 -0500

    even more pip installs

--- pip_installs.bat
+++ pip_installs.bat
@@ -1,5 +1,5 @@
 @echo off
 python -m pip install --upgrade pip
-python -m pip install google-generativeai python-dotenv nba-api pandas streamlit numpy plotly openai beautifulsoup4 nodejs
+python -m pip install google-generativeai python-dotenv nba-api pandas streamlit numpy plotly openai beautifulsoup4 nodejs tabulate
 npm install
 pause
